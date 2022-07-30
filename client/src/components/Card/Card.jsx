5b6d475d317715d61e0ea67887c0125ba8d4ee3b
Tokimekichika <toki174rus@gmail.com>
2022-07-31 03:03:49 +0500

    add card for jora

---
+++ client/src/components/Card/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Card () {
+    return (
+        <div>
+            Это твоя карта Гошан
+        </div>
+    );
+}
+
+export default Card;
--- client/src/containers/App/App.jsx
+++ client/src/containers/App/App.jsx
@@ -3,6 +3,7 @@ import StartPage from "../StartPage";
 import {BrowserRouter, Routes,Route} from "react-router-dom";
 import MainPage from "../MainPage/MainPage";
 import NavBar from "../../components/NavBar";
+import Card from "../../components/Card/Card";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
          <Routes>
              <Route path='/' element={<StartPage />} />
              <Route path='/main' element={<MainPage />} />
+             <Route path='/card' element={<Card/>} />
          </Routes>
      </BrowserRouter>
  </div>
